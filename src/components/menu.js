1647580abfc5c620ead5e1b37628594c2ae909f4
Joseph <sephxd1234@gmail.com>
2017-08-02 10:27:25 +0200

    Reworked badge display logic

--- src/components/menu.js
+++ src/components/menu.js
@@ -26,8 +26,8 @@ class Menu extends Component {
               Admin
             </NavItem>
             <NavItem eventKey={2} href="/cart">
-              Your Cart
-              <Badge className="ba">{this.props.cartItemsNumber}</Badge>
+              Your Cart {" "}
+              <Badge className="ba">{this.props.cartItemsNumber ? this.props.cartItemsNumber : ""}</Badge>
             </NavItem>
           </Nav>
         </Navbar.Collapse>
