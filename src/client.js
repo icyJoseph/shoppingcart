3ab851c001ce83375b1ba3f4a100669e76339cac
Joseph <sephxd1234@gmail.com>
2017-08-02 16:54:09 +0200

    Removed routing and imports. Imported Route from route.js

--- src/client.js
+++ src/client.js
@@ -10,11 +10,6 @@ import reducers from "./reducers/index";
 import * as booksActions from "./actions/booksActions";
 import * as cartActions from "./actions/cartActions";
 
-import BooksList from "./components/pages/booksList";
-import Cart from "./components/pages/cart";
-import BooksForm from "./components/pages/booksForm";
-import Main from "./main";
-
 import {
   Router,
   Route,
@@ -23,32 +18,17 @@ import {
   hashHistory
 } from "react-router";
 
-const middleware = applyMiddleware(thunk, logger);
-// Store
-const store = createStore(reducers, middleware);
+import routes from "./routes";
 
-// store.subscribe(function() {
-//   console.log("Current State is: ", store.getState());
-// });
+const middleware = applyMiddleware(thunk, logger);
+// Passing initial state from server to store
+const initialState = window.INITIAL_STATE;
+const store = createStore(reducers, initialState, middleware);
 
 const Routes = (
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={Main}>
-        <IndexRoute component={BooksList} />
-        <Route path="/admin" component={BooksForm} />
-        <Route path="/cart" component={Cart} />
-      </Route>
-    </Router>
+    {routes}
   </Provider>
 );
 
 ReactDOM.render(Routes, document.getElementById("app"));
-
-// store.dispatch(booksActions.deleteBook({ id: 2 }));
-
-// store.dispatch(cartActions.addToCart([{ id: 3 }]));
-
-// store.dispatch(
-//   booksActions.updateBook({ id: 1, title: "I gave you a new title" })
-// );
